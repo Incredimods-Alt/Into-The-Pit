f1370e38c8ccc1972bad2692c683d5ef368c001c
Incredi Dexer <157266806+IncrediDexer@users.noreply.github.com>
2024-08-09 17:03:36 -0400

    Add files via upload

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,106 +1,23 @@
-/* 0.5.4 2021-04-23 21:06:13 */
-var app = new function() {
-    this.name = "Alpha", this.version = "1", this.date = "2009", this.folder = "asset-v1/", this.looptime = 5333, this.bpm = 90, this.totalframe = 260, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !1, this.bonusendloopA = !1, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#121212", this.col0 = "#919191", this.col1 = "#5f5f5f", this.col2 = "#4b4b4b", this.col3 = "#373737", this.col4 = "#232323", this.animearray = [{
-        name: "1_lead",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "2_deux",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "3_kosh",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "4_shpok",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "5_tom",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "6_nouana",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "7_scratch",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "8_trill",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "9_bass",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "10_uh",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "11_nugu",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "12_guit",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "13_tromp",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "14_pouin",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "15_tung",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "16_aoun",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "17_hum",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "18_get",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "19_tellme",
-        color: "828282",
-        uniqsnd: !0
-    }, {
-        name: "20_make",
-        color: "828282",
-        uniqsnd: !0
-    }], this.bonusarray = [{
-        name: "The Choir",
-        src: "v1-b1-choir-hb.mp4",
-        code: "2,4,8,11,15",
-        sound: "bonus-choir",
-        aspire: "aspire"
-    }, {
-        name: "The Sailor",
-        src: "v1-b2-sailor-hb.mp4",
-        code: "10,11,14,15,20",
-        sound: "bonus-sailor",
-        aspire: "aspire-sailor"
-    }, {
-        name: "Santa",
-        src: "v1-b3-santa-hb.mp4",
-        code: "1,9,13,14,16",
-        sound: "bonus-santa",
-        aspire: "aspire-santa"
-    }];
-    for (var n = 0, o = this.animearray.length; n < o; n++) {
-        var a = this.animearray[n].name;
-        this.animearray[n].soundA = a + "_a", this.animearray[n].soundB = this.animearray[n].uniqsnd ? a + "_a" : a + "_b", this.animearray[n].anime = a + "-sprite.png", this.animearray[n].animeData = a + ".json"
-    }
-};
+var app=new function(){this.name="Into The Pit",this.version="v?",this.date="2024",this.folder="asset-v1/",this.looptime=6620,this.bpm=145,this.totalframe=316,this.nbpolo=7,this.nbloopbonus=2,this.bonusloopA=!0,this.bonusendloopA=!1,this.recmaxloop=34,this.recminloop=4,this.recmintime=Math.round(this.looptime/1e3)*this.recminloop,this.spritepolo="polo-sprite.png",this.spritepicto="game-picto.png",this.colBck="#090e30",this.col0="#5389A0",this.col1="#7FE2E2",this.col2="#7FE2E2",this.col3="#2B5B70",this.col4="#ffffff",this.animearray=[
+{name:"1_pete",color:"0077B6",uniqsnd:!1},
+{name:"2_michael",color:"0077B6",uniqsnd:!1},
+{name:"3_guard",color:"0077B6",uniqsnd:!1},
+{name:"4_jackie",color:"0077B6",uniqsnd:!1},
+{name:"5_cg5",color:"0077B6",uniqsnd:!1},
+{name:"6_thump",color:"802B5B",uniqsnd:!1},
+{name:"7_mark",color:"802B5B",uniqsnd:!1},
+{name:"8_ennard",color:"802B5B",uniqsnd:!1},
+{name:"9_mask",color:"802B5B",uniqsnd:!1},
+{name:"10_freddy",color:"802B5B",uniqsnd:!1},
+{name:"11_dheusta",color:"FFAA28",uniqsnd:!1},
+{name:"12_bonnie",color:"FFAA28",uniqsnd:!1},
+{name:"13_afton",color:"FFAA28",uniqsnd:!1},
+{name:"14_dawko",color:"FFAA28",uniqsnd:!0},
+{name:"15_monty",color:"FFAA28",uniqsnd:!1},
+{name:"16_edge",color:"A94915",uniqsnd:!1},
+{name:"17_pittie",color:"A94915",uniqsnd:!1},
+{name:"18_phoneguy",color:"A94915",uniqsnd:!1},
+{name:"19_mimic",color:"A94915",uniqsnd:!1},
+{name:"20_matpat",color:"A94915",uniqsnd:!1},
+{name:"21_puppet",color:"FFFFFF",uniqsnd:!1},
+],this.bonusarray=[{name:"Eyes",src:"bonus-eyes-hd.mp4",code:"2,6,9,15,18",sound:"bonus-eyes",aspire:"aspire-eyes"}],this.unlockerarray=[];for(var n=0,o=this.animearray.length;n<o;n++){var a=this.animearray[n].name;this.animearray[n].soundA=a+"_a",this.animearray[n].soundB=this.animearray[n].uniqsnd?a+"_a":a+"_b",this.animearray[n].anime=a+"-sprite.png",this.animearray[n].animeData=a+".json"}};
